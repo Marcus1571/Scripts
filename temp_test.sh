e937b056fcd4e4de735be73d37299393489e6855
Marcus1571 <48529859+Marcus1571@users.noreply.github.com>
2019-09-05 23:05:29 +0200

    Add files via upload

--- temp_test.sh
+++ temp_test.sh
@@ -4,8 +4,8 @@ clear
 
 for f in {1..7}
 do
-	vcgencmd measure_temp
-	sysbench --test=cpu --cpu-max-prime=25000 --num-threads=4 run >/dev/null 2>&1
+vcgencmd measure_temp
+sysbench --test=cpu --cpu-max-prime=25000 --num-threads=4 run >/dev/null 2>&1
 done
 
 vcgencmd measure_temp
